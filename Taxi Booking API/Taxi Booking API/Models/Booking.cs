285dfaba105654914662e72ec89f1cb5569d60e3
jonathan12352 <37117994+jonathan12352@users.noreply.github.com>
2022-03-14 10:38:33 +0800

    Update Booking Model with Destination property

--- Taxi Booking API/Taxi Booking API/Models/Booking.cs
+++ Taxi Booking API/Taxi Booking API/Models/Booking.cs
@@ -11,6 +11,7 @@ namespace Taxi_Booking_API.Models
         public DateTime Date { get; set; }
         public string Time { get; set; }
         public string PickupPoint { get; set; }
+        public string Destination { get; set; }
         public float Current_Location_Latitude { get; set; }
         public float Current_Location_Longitude { get; set; }
     }
